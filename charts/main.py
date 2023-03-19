abe362827ffcafc5e5426b67bcd9f197069909c3
aferuecas <adrianjobs010694@gmail.com>
2023-03-19 17:13:49 -0500

    Creacion de la carpeta app. Importacion de package matplotlib y de modulos creados por instructor de platzi para la generacion de imagenes de poblacion de un pais.

---
+++ charts/main.py
@@ -0,0 +1,11 @@
+import charts
+
+#Funcion principalñ
+def run():
+    charts.generate_piechart()#Llamamos, del m{odulo charts}, a la función
+
+#Entry point: Significa: Cuando se ejecute como un script ...
+if __name__ == '__main__':
+    run()
+
+
